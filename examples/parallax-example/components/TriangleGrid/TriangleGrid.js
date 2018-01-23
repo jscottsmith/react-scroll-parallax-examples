d3c80a9b508966caf6f7ee7e42fa4d779f761e89
J Smith <jscsmith@gmail.com>
2018-01-23 01:16:51 -0800

    update copy on example

--- examples/parallax-example/components/TriangleGrid/TriangleGrid.js
+++ examples/parallax-example/components/TriangleGrid/TriangleGrid.js
@@ -8,27 +8,37 @@ import angleTop from '!!raw-loader!../shared/angle-dark-top.svg';
 
 const TriangleGrid = () => (
     <div className={style.root}>
-        <Svg
-            svg={angleTop}
-            className={style.angleTop}
-        />
+        <Svg svg={angleTop} className={style.angleTop} />
         <article className={style.copy}>
             <p>
-                <code>npm i react-scroll-parallax</code>
+                Provides a React component and single passive scroll listener to
+                add vertical scrolling based offsets to elements based on their
+                position in the viewport. Works with universal (server-side
+                rendered) React apps.
+            </p>
+            <p className="small">
+                <code>npm i react-scroll-parallax --save</code>
+                <br />
+                <code>yarn add react-scroll-parallax</code>
             </p>
             <p>
-                <a className="btn" href="https://github.com/jscottsmith/react-scroll-parallax">View on GitHub</a>
+                <a
+                    className="btn"
+                    href="https://github.com/jscottsmith/react-scroll-parallax"
+                >
+                    View on GitHub
+                </a>
+                <a
+                    className="btn"
+                    href="https://www.npmjs.com/package/react-scroll-parallax"
+                >
+                    View on NPM
+                </a>
             </p>
         </article>
         <div className={style.container}>
-            <Parallax
-                offsetYMax={25}
-                offsetYMin={-25}
-            >
-                <Svg
-                    svg={gridPurple}
-                    className={style.trianglesPurple}
-                />
+            <Parallax offsetYMax={25} offsetYMin={-25}>
+                <Svg svg={gridPurple} className={style.trianglesPurple} />
             </Parallax>
             <Parallax
                 offsetYMax={50}
@@ -36,10 +46,7 @@ const TriangleGrid = () => (
                 offsetXMax={13}
                 offsetXMin={-13}
             >
-                <Svg
-                    svg={gridWhite}
-                    className={style.trianglesWhite}
-                />
+                <Svg svg={gridWhite} className={style.trianglesWhite} />
             </Parallax>
         </div>
     </div>
