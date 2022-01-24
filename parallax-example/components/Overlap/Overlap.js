ec4707bc5703193b2699c4608aee3deebbca9617
J Scott Smith <jscsmith@gmail.com>
2022-01-23 20:39:46 -0800

    chore: remove unused styles

--- parallax-example/components/Overlap/Overlap.js
+++ parallax-example/components/Overlap/Overlap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as style from "./Overlap.module.scss";
-import { Svg } from "../";
+import { Svg } from "..";
 import { Parallax } from "react-scroll-parallax";
 import circle from "bundle-text:./circle.svg";
 import circleRings from "bundle-text:./circle-rings.svg";
@@ -8,11 +8,7 @@ import circleRings from "bundle-text:./circle-rings.svg";
 const Overlap = () => (
   <div className={style.root}>
     <div className={style.container}>
-      <Parallax
-        translateY={[-50, 50]}
-        translateX={[-40, 20]}
-        className={style.circle}
-      >
+      <Parallax translateY={[-50, 50]} translateX={[-40, 20]}>
         <Svg svg={circle} />
       </Parallax>
       <Parallax
